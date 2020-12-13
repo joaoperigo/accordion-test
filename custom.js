8971dafc3b5bf4b5b14a3c49f29a0e3ccfec2dd7
joaoperigo <joperigo@gmail.com>
2020-12-13 19:36:50 -0300

    efeito automatico de filter css 3

--- custom.js
+++ custom.js
@@ -1,45 +1,34 @@
+//FILTER risk (may be better with css?)
 function recursion(c){
     setTimeout(function(c){
         c = c || 0;
-        console.log(c++);
+        $( ".img-destacada-home-js" ).css('filter', 'hue-rotate(' + c++ + 'deg)');
+        if(c==360) c=0;
         recursion(c);
-    },0,c);
+    },500,c);
 }
-
 recursion();
-
-//FILTER
-// function mudaCor(nFilter) {
-//     window.setInterval(function() {
-//         console.log(nFilter);
-//         $( ".img-destacada-home" ).css('filter', 'hue-rotate(' + nFilter + 'deg)');
-//         nFilter++;
-//         if(nFilter==360) nFilter=0;
-//         mudacor(nFilter);
-//     }, 1000);
-// }
-// window.setInterval(function() {
-//     mudaCor(0);
-// }, 1000);
-
-//ACCORDION
-// start open accordion
-// $('.filho').collapse({
-//     toggle: true
-//   })
 
 //abre filho
 $(window).on("load", function(){
     $( ".collapse-pai" ).on( "click", function() {
+        //
         $( this ).closest('.pai').find('.filho').collapse('toggle');
+        //
         $(this).closest('#accordion').find('.pai .show').collapse('toggle'); 
+        //
+        $(this).addClass('focus');
     });
 });
 
 //abre conteudo
 $(window).on("load", function(){
     $( ".collapse-filho" ).on( "click", function() {
+        //
         $(this).closest('.filho').find('.conteudo').collapse('toggle');
+        //
         $(this).closest('.pai').find('.filho .show').collapse('toggle');
+        //
+        $(this).addClass('focus');
     });
 });
